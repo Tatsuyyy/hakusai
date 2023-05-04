95c9a4c8aaa7df29115b45e08e89a5587d695b73
Tatsuyyy <aderybase910@gmail.com>
2023-05-05 00:02:09 +0900

    adminに各種モデル追加

--- hakusai/admin.py
+++ hakusai/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from hakusai.models import Actions, Exhibitions, Projects, Steps, ExhibitionSettings
 
 # Register your models here.
+admin.site.register(Actions)
+admin.site.register(Exhibitions)
+admin.site.register(Projects)
+admin.site.register(Steps)
+admin.site.register(ExhibitionSettings)
