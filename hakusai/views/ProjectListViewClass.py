34dacb88c3a3682a1c9615a848f69c622e8073eb
Tatsuyyy <aderybase910@gmail.com>
2023-05-16 22:39:40 +0900

    project_list作成

--- hakusai/views/ProjectListViewClass.py
+++ hakusai/views/ProjectListViewClass.py
@@ -1,5 +1,10 @@
+from typing import Any, Dict
 from django.views.generic import TemplateView
-
 
 class ProjectListView(TemplateView):
     template_name = 'hakusai/project_list.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'プロジェクト一覧'
+        return context
