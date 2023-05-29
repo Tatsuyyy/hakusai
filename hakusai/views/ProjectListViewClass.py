911b9a77670685d436e48e5c62532305d21a6289
Shieru093 <shieru093@gmail.com>
2023-05-30 00:53:48 +0900

    project_list.htmlとdbの連携

--- hakusai/views/ProjectListViewClass.py
+++ hakusai/views/ProjectListViewClass.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
 from django.views.generic import TemplateView
+from hakusai.models import Projects
 
 class ProjectListView(TemplateView):
     template_name = 'hakusai/project_list.html'
@@ -7,4 +8,6 @@ class ProjectListView(TemplateView):
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['title'] = 'プロジェクト一覧'
+        context['projects'] = Projects.objects.all()
         return context
+    
