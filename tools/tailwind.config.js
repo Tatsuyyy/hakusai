e79b02ba20c0bda751ee8cb4efda42163dee068a
Tatsuyyy <aderybase910@gmail.com>
2023-05-31 20:21:19 +0900

    tailwindの読込先を変更

--- tools/tailwind.config.js
+++ tools/tailwind.config.js
@@ -8,8 +8,16 @@ module.exports = {
     content: ['../**/templates/*.html', '../**/templates/**/*.html'],
   },
   theme: {
-    extend: {},
-  },
-  variants: {},
+    extend: {
+        spacing:{
+          '1/6': '16.6666667%',
+          '2/6': '33.3333333%',
+          '3/6': '50%',
+          '4/6': '66.6666667%',
+          '5/6': '83.3333333%',
+        }
+      }
+    },
+    variants: {},
   plugins: [],
 }
