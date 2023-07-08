9472270588d1ee812d9e35534cb718d262cc1645
Tatsuyyy <aderybase910@gmail.com>
2023-07-08 17:24:41 +0900

    展示一覧の画面作成

--- hakusai/models.py
+++ hakusai/models.py
@@ -31,6 +31,7 @@ class Exhibitions(Model):
     name = models.CharField(max_length=100, verbose_name='展示名')
     memo = models.CharField(max_length=300, verbose_name='メモ')
     created_date_time = models.DateTimeField(verbose_name='作成日時')
+    delete_flag = models.BooleanField(default=False, verbose_name='削除フラグ')
 
     def save(self, *args, **kwargs):
         if not self.id:
