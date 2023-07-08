6ab7d3375dfc31a612ce1c4d5310219d676fe17a
Tatsuyyy <aderybase910@gmail.com>
2023-07-08 16:37:39 +0900

    プロジェクト編集ページのTemplate作成

--- hakusai/views/ProjectEditViewClass.py
+++ hakusai/views/ProjectEditViewClass.py
@@ -1,5 +1,18 @@
+from typing import Any, Dict
+from django import http
 from django.views.generic import TemplateView
-
 
 class ProjectEditView(TemplateView):
     template_name = 'hakusai/project_edit.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        return context
+
+    # POST送信を受け取ったときの処理
+    def post(self, request: http.HttpRequest, *args: Any, **kwargs: Any) -> http.HttpResponse:
+        print(request.POST)
+        # メモ(最終的には消してね)
+        # postで値送信を受け取り(request.POSTを要確認)、それを使ってstepsを更新する
+        # その後returnでプロジェクト一覧にページ遷移する
+        return super().get(request, *args, **kwargs)
