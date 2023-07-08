af4481f8c646df650e2655625d1fbf85009236bf
Tatsuyyy <aderybase910@gmail.com>
2023-07-08 19:55:50 +0900

    展示実行ページの画面作成

---
+++ static/hakusai/js/exhibition_run.js
@@ -0,0 +1,35 @@
+window.onload = () => {
+    registerButtonEvents();
+};
+
+const registerButtonEvents = () => {
+    const startButton = document.getElementById("js-start-button");
+    const stopButton = document.getElementById("js-stop-button");
+    const statusText = document.getElementById("js-exhibition-status");
+
+    startButton.addEventListener("click", () => {
+        stopButton.removeAttribute("disabled");
+        startButton.children[0].classList.replace("text-green-500", "text-gray-400");
+        startButton.children[1].classList.replace("text-gray-700", "text-gray-400");
+        stopButton.children[0].classList.replace("text-gray-400", "text-red-600");
+        stopButton.children[1].classList.replace("text-gray-400", "text-gray-700");
+        statusText.innerText = "を再生中...";
+        startButton.setAttribute("disabled", true);
+    });
+
+    stopButton.addEventListener("click", () => {
+        startButton.removeAttribute("disabled");
+        stopButton.children[0].classList.replace("text-red-600", "text-gray-400");
+        stopButton.children[1].classList.replace("text-gray-700", "text-gray-400");
+        startButton.children[0].classList.replace("text-gray-400", "text-green-500");
+        startButton.children[1].classList.replace("text-gray-400", "text-gray-700");
+        statusText.innerText = "を停止中";
+        stopButton.setAttribute("disabled", true);
+    });
+
+    startButton.children[0].classList.replace(
+      "text-green-500",
+      "text-gray-400"
+    );
+
+};
