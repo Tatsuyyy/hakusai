b68e87b5ed3c18c15ee841a43c7c5817d9a40d4f
Shieru093 <shieru093@gmail.com>
2023-09-25 19:31:05 +0900

    ステップ数が減少した際の処理の追加

--- hakusai/views/ProjectEditViewClass.py
+++ hakusai/views/ProjectEditViewClass.py
@@ -21,10 +21,6 @@ class ProjectEditView(TemplateView):
 
     # POST送信を受け取ったときの処理
     def post(self, request: http.HttpRequest, *args: Any, **kwargs: Any) -> http.HttpResponse:
-        print(request.POST)
-        # メモ(最終的には消してね)
-        # postで値送信を受け取り(request.POSTを要確認)、それを使ってstepsを更新する
-        # その後returnでプロジェクト一覧にページ遷移する
         current_project_id = self.kwargs['project_id'] # 編集中のprojectのid
 
         # 更新前のstep数
@@ -32,21 +28,21 @@ class ProjectEditView(TemplateView):
         # 更新後のstep数
         after_step_count = sum(1 for key in request.POST if key.endswith('xpath'))
         # 更新前後のstep数を比較し、大きいほうをループ回数用変数に代入
-        # loop_count = after_step_count if after_step_count >= before_step_count else before_step_count
-        loop_count = after_step_count
+        loop_count = max(before_step_count, after_step_count)
 
         for i in range(1, loop_count + 1):
             # stepの更新または追加の処理
-            # if i <= after_step_count:
+            if i <= after_step_count:
                 action = Actions.objects.get(name=request.POST.get(f'step{i}-action-name'))  # action名からレコードを取得
                 # project_idと順序でレコードを検索してヒットすれば上書き、なければ追加
                 Steps.objects.update_or_create(project_id=current_project_id, exec_order=i, defaults={'xpath': request.POST.get(f'step{i}-xpath'), 'action_str': request.POST.get(f'step{i}-action-str'), 'action_id': action.id})
             # 使わなくなったstepを初期化する処理
-            # else:
-                # 使わなくなったレコードの内容をNoneで初期化
-                # Steps.objects.filter(project_id=current_project_id, exec_order=i).update(xpath=None, action_str=None, action_id=None)
+            else:
+                # 使わなくなったレコードはproject_idを空に上書き
+                Steps.objects.filter(project_id=current_project_id, exec_order=i).update(project_id=None)
 
         current_project = Projects.objects.get(id=current_project_id)
+        # クリックされたボタンの状態を取得し対応する処理を実行
         submit_type = request.POST.get('submit-type')
         if submit_type == 'cancel':
             current_project.delete_flag = True
@@ -58,4 +54,4 @@ class ProjectEditView(TemplateView):
             current_project.delete_flag =False
             current_project.draft_flag = False
         current_project.save()
-        return redirect('hakusai:project_list')
+        return redirect('hakusai:project_list') # プロジェクト一覧画面に遷移
